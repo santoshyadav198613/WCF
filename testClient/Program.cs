dbbffbf70f7ca037d91209a29cb79ab7570bbc9b
yadav.santosh <yadav.santosh@IPAD81043-PC>
2016-10-31 17:22:09 +0530

    Add project files.

---
+++ testClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using testClient.testService;
+
+namespace testClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Service1Client client = new Service1Client())
+            {
+                Book b = new Book() { Author = "test3", ID = 3, Name = "test4" };
+                List<Book> book = client.GetBooks(b);
+                Console.WriteLine("Book Count" + book.Count);
+                Book b1 = new Book() { Author = "test4", ID = 3, Name = "test5" };
+                List<Book> book1 = client.GetBooks(b1);
+                Console.WriteLine("Book Count" + book1.Count);
+                Console.ReadLine();
+            }
+        }
+    }
+}
